91d7306c8beb52a2511c2ab0acd1dfae8dafd6cd
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-03-11 14:23:43 +0330

    Rename module name

--- negare/asgi.py
+++ negare/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Negare project.
+ASGI config for negare project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Negare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'negare.settings')
 
 application = get_asgi_application()
--- negare/wsgi.py
+++ negare/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Negare project.
+WSGI config for negare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Negare.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'negare.settings')
 
 application = get_wsgi_application()
