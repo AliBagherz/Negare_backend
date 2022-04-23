47851c72e2292d1b824c044dc7590d607664efd6
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-04-23 17:49:01 +0430

    Fix pylint error

--- negare/negare/urls.py
+++ negare/negare/urls.py
@@ -23,7 +23,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+SchemaView = get_schema_view(
    openapi.Info(
       title="Snippets API",
       default_version='v1',
@@ -40,7 +40,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        SchemaView.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    re_path(
+        r'^swagger/$',
+        SchemaView.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
 ]
