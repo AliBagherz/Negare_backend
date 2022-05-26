4e75be45c585ae6e1b840f1589f0fac3144f9ed4
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-05-26 22:18:04 +0430

    Add message receiver

--- negare/chat/consumers.py
+++ negare/chat/consumers.py
@@ -1,6 +1,33 @@
+import json
+
 from channels.consumer import AsyncConsumer
 
 
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print('Connected', event)
+        await self.send(
+            {
+                'type': 'websocket.accept'
+            }
+        )
+
+    async def websocket_receive(self, event):
+        print('receive', event)
+        received_data = json.loads(event['text'])
+        _type = received_data.get('type')
+
+        if not _type:
+            return False
+
+        # ToDo: handle message models
+
+        await self.send(
+            {
+                'type': 'websocket.send',
+                'text': json.dumps({})
+            }
+        )
+
+    async def websocket_disconnect(self, event):
+        print('disconnect', event)
--- negare/negare/asgi.py
+++ negare/negare/asgi.py
@@ -9,13 +9,21 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'negare.settings')
 
 application = ProtocolTypeRouter(
     {
-        'http': get_asgi_application()
+        'http': get_asgi_application(),
+        'websocket': AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
     }
 )
