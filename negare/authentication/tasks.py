178cff1af79570a01901aa0189822b21fc69ec0c
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-05-27 23:03:30 +0430

    some improvement on email verifier

--- negare/authentication/tasks.py
+++ negare/authentication/tasks.py
@@ -1,7 +1,4 @@
-
 from django.core.mail import send_mail
-from django.core.validators import validate_email
-
 from authentication.models import AppUser
 from authentication.utils import get_otp_code
 from negare.celery import app
@@ -10,14 +7,8 @@ from negare.settings import EMAIL_HOST_USER
 
 @app.task(bind=True)
 def send_email(self, *args):
-    print('salam')
     user = AppUser.objects.get(pk=args[0])
-    print(user.id)
     otp_code = get_otp_code(args[0])
-
-    if not validate_email(user.email):
-        raise Exception('Email InValid')
-
     send_mail(
         'OTP Code - Negare',
         f'Hi {user.first_name}!\nYour otp_code is: {otp_code}',
