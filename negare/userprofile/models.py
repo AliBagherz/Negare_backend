728ed79116c5dbcf82db9ccd52070f2679c9ebc5
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-06-17 19:57:04 +0430

    Add get, put, patch of profile Api

--- negare/userprofile/models.py
+++ negare/userprofile/models.py
@@ -10,20 +10,12 @@ GENDER_CHOICES = (
 )
 
 
-class ProfileBase(BaseModel):
-    first_name = models.CharField(max_length=20, null= True, blank=True)
-    last_name = models.CharField(max_length=20, null= True, blank=True)
+class UserProfile(BaseModel):
     phone_number = models.CharField(max_length=11, null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=1, default='N')
     national_code = models.CharField(max_length=10, null=True, blank=True)
     birthdate = models.CharField(max_length=10, null=True, blank=True)
 
-
-    class Meta:
-        abstract = True
-
-
-class UserProfile(ProfileBase):
     user = models.OneToOneField(
         AppUser, on_delete=models.CASCADE, related_name="user_profile"
     )
