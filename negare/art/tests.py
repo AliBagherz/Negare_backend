f4f8502351290b66731e3cb42fce55fbafe051c9
AliBagherz <Ali.Bagherzadeh@digikala.com>
2022-07-10 20:13:49 +0430

    Add 11 tests for art pieces APis

--- negare/art/tests.py
+++ negare/art/tests.py
@@ -8,6 +8,7 @@ from authentication.models import AppUser
 
 from art.models import ArtTypeChoice, ArtPiece
 from core.models import Image
+from userprofile.models import UserProfile
 
 
 class ArtTests(TestCase):
@@ -19,6 +20,7 @@ class ArtTests(TestCase):
         self.user = AppUser.objects.create_user(username='test_user', password='12345678')
         self.user.is_active = True
         self.user.save()
+        UserProfile.objects.create(user=self.user)
         self.client.force_authenticate(self.user)
 
         self.image = Image.objects.create(image=ImageFile(open("./test_images/test.png", "rb")))
