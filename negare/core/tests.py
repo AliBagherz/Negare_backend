717afcc8f93272389fb27a66803f66596e3deb69
pouria-zamani <pouriaxz2001@gmail.com>
2022-07-11 11:29:02 +0430

    add core tests

--- negare/core/tests.py
+++ negare/core/tests.py
@@ -1,3 +1,24 @@
+from django.core.files.images import ImageFile
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
 from django.test import TestCase
 
-# Create your tests here.
+from authentication.models import AppUser
+from art.models import ArtTypeChoice, ArtPiece
+from core.models import Image
+from userprofile.models import UserProfile
+
+
+class CoreTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        # create user
+        self.user = AppUser.objects.create_user(username='test_user', password='12345678')
+        self.user.is_active = True
+        self.user.save()
+        UserProfile.objects.create(user=self.user)
+        self.client.force_authenticate(self.user)
+
+        self.image = Image.objects.create(image=ImageFile(open("./test_images/test.png", "rb")))
